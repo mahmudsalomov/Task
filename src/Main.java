fa6ddcdbb2980a44e43b8fd210a7c8f7546299e0
Mahmud Salomov <mahmud.salomov.salomov@gmail.com>
2023-01-11 08:58:17 +0500

    modified

--- src/Main.java
+++ src/Main.java
@@ -22,11 +22,61 @@ public class Main {
             System.out.println(boxList.get(maxSequence));
         }
     }
+    static List<Integer> getMaxSequence(List<Box> boxes) {
+        List<List<Integer>> indexes=new ArrayList<>();
+
+        System.out.println("Given boxes:");
+        System.out.println(boxes);
+
+        boxes.sort((a, b) -> (b.height * b.width - a.height * a.width));
+
+        System.out.println("Sorted boxes:");
+        System.out.println(boxes);
+        int[] maxHeight = new int[boxes.size()];
+        for(int i = 0; i<boxes.size(); i++)
+            maxHeight[i] = boxes.get(i).height;
+
+        for(int i = 0;i<boxes.size();i++) {
+            List<Integer> a=new ArrayList<>();
+
+            Box curr = boxes.get(i);
+            int val = 0;
+
+            for(int j=0;j<i;j++) {
+                Box temp = boxes.get(j);
+                if(temp.width > curr.width && temp.depth > curr.depth){
+                    if (val<maxHeight[j]) {
+                        a.add(j);
+                        val = maxHeight[j];
+                    }
+                }
+            }
+
+            if (i==boxes.size()-1) a.add(i);
+
+            indexes.add(a);
+
+            maxHeight[i] += val;
+        }
+        return maxSequenceIndexList(indexes);
+    }
+
+    public static List<Integer> maxSequenceIndexList(List<List<Integer>> list){
+        int maxSize=list.get(0).size();
+        int maxSizeIndex=0;
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i).size()>maxSize) {
+                maxSize=list.get(i).size();
+                maxSizeIndex=i;
+            }
+        }
+        return list.get(maxSizeIndex);
+    }
+
     static class Box {
 
         int height, width, depth, area;
-        Box(int h, int w, int d)
-        {
+        Box(int h, int w, int d) {
             this.height = h;
             this.width = w;
             this.depth = d;
@@ -43,55 +93,5 @@ public class Main {
                     '}';
         }
 
-    }
-    static List<Integer> getMaxSequence(List<Box> boxes)
-    {
-        List<List<Integer>> indexes=new ArrayList<>();
-
-        System.out.println("Given boxes:");
-        System.out.println(boxes);
-
-        boxes.sort((a, b) -> (b.height * b.width - a.height * a.width));
-
-        System.out.println("Sorted boxes:");
-        System.out.println(boxes);
-        int[] maxHeight = new int[boxes.size()];
-        for(int i = 0; i<boxes.size(); i++) maxHeight[i] = boxes.get(i).height;
-        for(int i = 0;i<boxes.size();i++)
-        {
-            List<Integer> a=new ArrayList<>();
-
-            Box curr = boxes.get(i);
-            int val = 0;
-
-            for(int j=0;j<i;j++)
-            {
-                Box temp = boxes.get(j);
-                if(temp.width > curr.width && temp.depth > curr.depth){
-                    if (val<maxHeight[j]) a.add(j);
-                    val = Math.max(val, maxHeight[j]);
-                }
-            }
-
-            if (i==boxes.size()-1) a.add(i);
-            indexes.add(a);
-
-            maxHeight[i] += val;
-        }
-        return maxSequenceIndexList(indexes);
-    }
-
-
-
-    public static List<Integer> maxSequenceIndexList(List<List<Integer>> list){
-        int maxSize=list.get(0).size();
-        int maxSizeIndex=0;
-        for (int i = 1; i < list.size(); i++) {
-            if (list.get(i).size()>maxSize) {
-                maxSize=list.get(i).size();
-                maxSizeIndex=i;
-            }
-        }
-        return list.get(maxSizeIndex);
     }
 }
